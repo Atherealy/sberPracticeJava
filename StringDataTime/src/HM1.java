7db0aeef931046d7be7d9d52b8640abb7a587774
Atherealy <hfggklm@gmail.com>
2023-04-03 23:46:18 +0300

    Домашняя работа по Строки, дата и время

---
+++ StringDataTime/src/HM1.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.Scanner;
+
+public class HM1 {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        String n = reader.next();
+        String[] stringarray = n.split("");
+
+        Map<String, Long> map = Arrays.stream(stringarray).collect(Collectors.groupingBy(c -> c , Collectors.counting()));
+        map.forEach((k, v) -> System.out.println(k + " : "+ v));
+    }
+}
---
+++ StringDataTime/src/HM5.java
@@ -0,0 +1,19 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Scanner;
+
+public class HM5 {
+    private static final DateTimeFormatter format = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+    public static long Age(String Now, String Birthday) {
+
+        return Math.abs(ChronoUnit.YEARS.between(LocalDate.parse(Birthday, format), LocalDate.parse(Now, format)));
+    }
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        String now = reader.next();
+        String birth = reader.next();
+        System.out.println(Age(now, birth));
+    }
+}
